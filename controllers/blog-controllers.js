5eb920ce90d2dc29d549be4f6d045e3f1b83fa5f
Albert Osei Frimpong <alotab@yahoo.com>
2025-01-06 19:17:48 -0800

    update routes

--- controllers/blog-controllers.js
+++ controllers/blog-controllers.js
@@ -49,10 +49,128 @@ const addBlog = async(req, res)=> {
     }
 };
 
+const getBlogById = async(req, res)=>{
+    try{
+        const getCurrentBlogId = req.params.id;
+        const blogDetailsById = await Blog.findById(getCurrentBlogId);
+
+        if(!blogDetailsById){
+            return res.status(404).json({
+                success : false,
+                message : `Blog with current ID: ${getCurrentBlogId} not found. Please try with a different ID`
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            data : blogDetailsById
+        })
+
+    }catch(error){
+        console.log('Error', e);
+        res.status(500).json({
+            success : false,
+            message: 'Something went wrong! Please try again'
+        });
+    }
+};
+
+const updateBlog = async(req, res)=>{
+    try{
+        // Get the userId from the decoded JWT (from the authMiddleware)
+        const userId = req.userInfo.userId;
+
+        // Get the updated blog data from the request body
+        const updateBlogFormData = req.body;
+
+        // Get the blog ID from the URL params
+        const getCurrentBlogId = req.params.id;
+
+        // Find the current blog by its ID
+        const blog = await Blog.findById(getCurrentBlogId);
+
+        // Check if the logged-in user is the author of the blog
+        if (blog.author.toString() !== userId) {
+            return res.status(403).json({
+                success :false,
+                message : 'You are not authorzed to update this blog. Only the author can modify it.'
+            })
+        }
+
+        // Update the blog if the user is the author
+        const updateBlog = await Blog.findByIdAndUpdate(
+            getCurrentBlogId,
+            updateBlogFormData,
+            {
+                new : true,
+            }
+        );
+
+        // Return success response with updated blog data
+        res.status(200).json({
+            success: true,
+            message: 'Blog updated successfully',
+            data: updateBlog
+        });
+
+    }catch(error){
+        console.log('Error', error);
+        res.status(500).json({
+            success : false,
+            message: 'Something went wrong! Please try again'
+        });
+    }
+};
+
+const deleteBlog =async(req, res)=>{
+    try{
+
+        // Get the userId from the decoded JWT (from the authMiddleware)
+        const userId = req.userInfo.userId;
+
+        // Get the blog ID from the URL params
+        const getBlogId = req.params.id;
+
+        // Find the current blog by its ID
+        const blog = await Blog.findById(getBlogId);
+
+        if(!blog) {
+            res.status(404).json({
+                success: false,
+                message: `No Blog with ID: ${getBlogId} found in the database`
+            })
+        }
+
+        // Check if the logged-in user is the author of the blog
+        if (blog.author.toString() !== userId) {
+            return res.status(403).json({
+                success :false,
+                message : 'You are not authorzed to delete this blog. Only the author can delete it.'
+            });
+        }
+
+        const deleteBlog = await Blog.findByIdAndDelete(getBlogId);
+        res.status(200).json({
+            success : true,
+            message: "Blog is deleted successfully",
+            data : deleteBlog
+        });
+
+    }catch(error){
+        console.log('Error', error);
+        res.status(500).json({
+            success : false,
+            message: 'Something went wrong! Please try again'
+        });
+    };
+    
+};
 
 
-
-
-
-
-module.exports = {fetchAllBlogs, addBlog}
+module.exports = {
+    fetchAllBlogs, 
+    addBlog, 
+    getBlogById, 
+    updateBlog, 
+    deleteBlog 
+}
--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {fetchAllBlogs, addBlog} = require('../controllers/blog-controllers');
+const {fetchAllBlogs, addBlog, getBlogById, updateBlog, deleteBlog } = require('../controllers/blog-controllers');
 const authMiddleware = require('../middleware/auth-middleware')
 // Create express router
 const router = express.Router()
@@ -9,6 +9,9 @@ const router = express.Router()
 // all routes that are related to Blog only
 router.get('/get', fetchAllBlogs);
 router.post('/add', authMiddleware, addBlog);
+router.get('/get/:id', getBlogById);
+router.put('/update-blog/:id', authMiddleware, updateBlog);
+router.delete('/delete/:id', authMiddleware, deleteBlog);
 
 
 module.exports = router;
